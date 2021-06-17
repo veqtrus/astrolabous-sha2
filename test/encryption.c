4c81f826fb2da1cb6b5d5bde6de9be5bdeb38ace
Paul Georgiou <pavlos1998@gmail.com>
2021-06-17 03:34:27 +0100

    Measure duration and speed in encryption test

--- test/encryption.c
+++ test/encryption.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,27 +15,35 @@ int main(int argc, char **argv)
 	uint8_t *witness;
 	uint32_t n_chain = 1024;
 	uint64_t n_iter = 1024;
+	uint64_t n_hashes;
+	clock_t start;
+	double duration;
 	if (argc == 3) {
 		n_chain = atoi(argv[1]);
 		n_iter = 1;
 		n_iter <<= atoi(argv[2]);
 	}
+	n_hashes = n_iter * n_chain;
 	cipher = malloc(astrolabous_cipher_size(n_chain));
 	witness = malloc(astrolabous_witness_size(n_chain));
+	start = clock();
 	err_str = astrolabous_encrypt_key(key, cipher, witness, n_chain, n_iter);
+	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	if (err_str) {
 		fprintf(stderr, "%s\n", err_str);
 		ret = 1;
 		goto exit;
 	}
-	puts("Encrypted");
+	printf("Encrypted in %f s (%f MH/s)\n", duration, n_hashes * 1e-6 / duration);
+	start = clock();
 	astrolabous_recover_key(decr_key, cipher, n_chain, n_iter);
-	puts("Decrypted");
+	duration = (clock() - start) / (double) CLOCKS_PER_SEC;
 	if (memcmp(key, decr_key, ASTROLABOUS_KEY_SIZE)) {
 		fprintf(stderr, "Keys do not match\n");
 		ret = 2;
 		goto exit;
 	}
+	printf("Recovered in %f s (%f MH/s)\n", duration, n_hashes * 1e-6 / duration);
 exit:
 	free(err_str);
 	free(cipher);
